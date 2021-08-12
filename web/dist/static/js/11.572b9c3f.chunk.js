088b22679428ae89dbdd8915ef579b68bd5cf95e
Nicholas Browning <systems@telegeography.com>
2021-08-12 16:03:07 -0400

    Remove dist directory

--- web/dist/static/js/11.572b9c3f.chunk.js
+++
@@ -1,7 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{633:
-/*!************************************************!*\
-  !*** ./src/pages/NotFoundPage/NotFoundPage.js ***!
-  \************************************************/
-/*! exports provided: default */
-/*! all exports used */
-/*! ModuleConcatenation bailout: Module is referenced from these modules with unsupported syntax: ./src/Routes.js (referenced with import()) */function(e,t,a){"use strict";a.r(t);var l=a(/*! react */0),n=a.n(l),m=a(/*! @redwoodjs/router */6),r=a(/*! @heroicons/react/outline */12);t.default=function(){return n.a.createElement("div",{className:"w-full h-screen bg-tg-blue-10"},n.a.createElement("div",{className:"w-full md:w-1/2 px-4 mx-auto pt-48"},n.a.createElement("div",{className:"w-full md:w-112"},n.a.createElement("h1",{className:"text-4xl mb-4"},"You have ventured into uncharted territory."),n.a.createElement("p",{className:"text-xl mb-8"},"You may have mistyped the address or the page may have moved."),n.a.createElement(m.Link,{to:m.routes.home(),className:"font-bold inline-block leading-5 my-1.5 py-3 px-4 rounded-md bg-tg-blue-30 text-tg-blue"},n.a.createElement(r.a,{className:"w-5 h-5 mr-1 float-left"}),n.a.createElement("span",null,"Return to the map")))))}}}]);
